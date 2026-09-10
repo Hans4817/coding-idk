3f3b18a194555b50f724e5ddaf2ecb075fc7afd9
Hans4817 <194430110+Hans4817@users.noreply.github.com>
2026-09-10 21:49:39 +0200

    added binary

--- Thien Long/Important Stuff/Coding/WebTechnologie/HTML-2026/Javascript/Calculation/Binary/Convertion.js
+++ Thien Long/Important Stuff/Coding/WebTechnologie/HTML-2026/Javascript/Calculation/Binary/Convertion.js
@@ -2,10 +2,28 @@ const binaryText = document.getElementById("binary");
 const decimalText = document.getElementById("decimal");
 
 let decimalInput = parseInt(window.prompt("Geben Sie Dezimalzahl ein: "));
-console.log(decimalInput);
-if(decimalInput >= 1024) console.error("Error while passing value: decimal input is over 1024.");
+
+
+// if(decimalInput >= 1024) console.error("Error while passing value: decimal input is over 1024.");
+
+let array = {};
+for(let i = 10; i >= 0; i--) { // initialize array for pow(2, 1) till pow(2, 10)
+    array[i] = Math.pow(2, i);
+}
+
+let bits = {};
 
 let i = 10;
-while(true) {
-    //if(decimalInput)
+while(decimalInput > 0 && i != 0) {
+    if(decimalInput >= array[i]) {
+        bits[i] = 1;
+
+        decimalInput -= array[i];
+    }
+    else {
+        bits[i] = 0;
+    }
+    i--;
 }
+
+console.log(bits);
