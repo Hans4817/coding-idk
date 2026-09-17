891ea346428a41082be76da423d837689093a154
Hans4817 <194430110+Hans4817@users.noreply.github.com>
2026-09-17 19:42:40 +0200

    added binary convertion

--- Thien Long/Important Stuff/Coding/WebTechnologie/HTML-2026/Javascript/Calculation/Binary/Convertion.js
+++ Thien Long/Important Stuff/Coding/WebTechnologie/HTML-2026/Javascript/Calculation/Binary/Convertion.js
@@ -1,29 +1,35 @@
-const binaryText = document.getElementById("binary");
-const decimalText = document.getElementById("decimal");
+let result = document.getElementById("result");
 
 let decimalInput = parseInt(window.prompt("Geben Sie Dezimalzahl ein: "));
+let decimalTempelate = decimalInput.toString();
 
-
-// if(decimalInput >= 1024) console.error("Error while passing value: decimal input is over 1024.");
-
-let array = {};
+let array = [];
 for(let i = 10; i >= 0; i--) { // initialize array for pow(2, 1) till pow(2, 10)
     array[i] = Math.pow(2, i);
 }
 
-let bits = {};
+let bits = [];
 
 let i = 10;
-while(decimalInput > 0 && i != 0) {
+let start = false;
+while(i !== -1) {
     if(decimalInput >= array[i]) {
         bits[i] = 1;
-
+        start = true;
         decimalInput -= array[i];
     }
-    else {
+    else if(start) {
         bits[i] = 0;
     }
     i--;
 }
 
-console.log(bits);
+let output = "";
+result.textContent = "Decimal: " + decimalTempelate + " | Binaer: ";
+
+// this is the output of the bits array
+for(let i = bits.length - 1; i >= 0; i--) {
+    output += bits[i];
+}
+console.log(output);
+result.textContent += output;
